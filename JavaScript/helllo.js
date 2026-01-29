98e5d20dae8170dfda2f7fc3398bdef7dc973234
kamrul25 <kamrul.coder01@gmail.com>
2026-01-29 14:55:10 +0600

    swap value bys using destructuring

--- JavaScript/helllo.js
+++ JavaScript/helllo.js
@@ -1,3 +1,12 @@
 console.log("Hello World!");
 
 console.log("Hi I am Kamrul Hasan Jaman");
+
+
+// Easy swapping by using destructuring
+
+let a = 12, b = 5, c = 20;
+
+[a, b, c] = [b, c, a]
+
+console.log(a, b , c);
